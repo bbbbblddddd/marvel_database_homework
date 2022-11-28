bfb763e6aaf99ed27225b4bbfe2bf12b9e835123
Brian Dickson <brianlawrencedickson@gmail.com>
2022-11-28 19:21:42 +0000

    extensions commit

--- marvel.sql
+++ marvel.sql
@@ -69,6 +69,9 @@ SELECT show_time FROM movies WHERE title = ('Iron Man 2');
 
 UPDATE movies SET show_time = '20.45' WHERE title = ('Iron Man 3');
 
+-- deleted all movies from 2017
+DELETE FROM movies WHERE year = 2017;
+
 SELECT * FROM movies;
 
 
